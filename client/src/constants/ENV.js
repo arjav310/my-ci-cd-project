08290c7f42289a6ef248b888f3e9dc34ed4c9808
arjav310 <arjav.patel97@gmail.com>
2023-04-16 13:50:04 -0600

    Port Update

--- client/src/constants/ENV.js
+++ client/src/constants/ENV.js
@@ -1,1 +1,1 @@
-export default { API_BASE_URL: "http://localhost:8080/api/todo" };
+export default { API_BASE_URL: "http://localhost:8081/api/todo" };
--- server/src/index.js
+++ server/src/index.js
@@ -8,14 +8,13 @@ const logger = require("./core/Logger");
 const todoRoute = require("./routes/todo.route");
 const loggerMiddleware = require("./middlewares/logger.middleware");
 const app = express();
-const port = 8080;
+const port = 8081;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 app.use(cors({ origin: "*" }));
-
 
 app.use(loggerMiddleware.logger());
 
