66a4ce27ddef443f2c50bcd5f77fc4d92488c2cc
arjav310 <arjav.patel97@gmail.com>
2023-04-21 10:05:58 -0600

    Title changed

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ const App = () => {
     <MainContext.Provider value={states}>
       <div className="app-container">
         <Semantic.Segment className="list-container">
-          <Title text="Todo - New" />
+          <Title text="Todo App - by Arjav" />
           <Form />
           <Semantic.Divider />
           <div className="app-item-list">
